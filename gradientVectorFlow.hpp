c2d2a42cb31d0d1c7437d2350bb1a8958f813afb
Erik Smistad <smistad@idi.ntnu.no>
2013-12-30 19:35:04 +0100

    added GVF cpp opencl code

---
+++ gradientVectorFlow.hpp
@@ -0,0 +1,10 @@
+#ifndef GVF_H
+#define GVF_H
+#include "commons.hpp"
+#include "SIPL/Types.hpp"
+#include "parameters.hpp"
+using namespace cl;
+
+Image3D runFMGGVF(OpenCL &ocl, Image3D *vectorField, paramList &parameters, SIPL::int3 &size);
+
+#endif
